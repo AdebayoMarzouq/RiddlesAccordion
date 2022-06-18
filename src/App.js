d8e5fd4f7d4a7e854e6023aa31adc7342aee561d
Marzouq <marzouqdevmail@gmail.com>
2022-06-18 14:53:57 +0100

    saved data to state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react'
 import data from './data';
 import SingleQuestion from './Question';
 
 function App() {
+  const [riddles, setRiddles] = useState(data)
+
   return (
     <main>
       <h1>Your Dose Of Funny Riddles</h1>
       <div className="faq-container">
-        {data.map(data =>
+        {riddles.map(data =>
           <SingleQuestion key={data.id} {...data} />
         )}
       </div>
