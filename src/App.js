20c403bb32042b4bf97c78f1a57ec3e532e35adf
Marzouq <marzouqdevmail@gmail.com>
2022-06-18 15:31:00 +0100

    added css autoprefixers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import data from './data';
-import SingleQuestion from './Question';
+import data from './data'
+import SingleQuestion from './Question'
 
 function App() {
   const [riddles, setRiddles] = useState(data)
@@ -8,13 +8,13 @@ function App() {
   return (
     <main>
       <h1>Your Dose Of Funny Riddles</h1>
-      <div className="faq-container">
-        {riddles.map(data =>
+      <div className='faq-container'>
+        {riddles.map((data) => (
           <SingleQuestion key={data.id} {...data} />
-        )}
+        ))}
       </div>
     </main>
   )
 }
 
-export default App;
+export default App
